74f6453f3b25334a5d3af6f50e9aa6202e1ca30b
ChisleLP <mischa.grothmann@hotmail.de>
2021-12-12 16:34:09 +0100

    Party first release

--- src/main/java/de/freddo/party/Enums.java
+++ src/main/java/de/freddo/party/Enums.java
@@ -2,12 +2,6 @@ package de.freddo.party;
 
 public class Enums {
 
-    public enum Errors {
-        OK,
-        In_Party,
-        Target_not_party
-    }
-
     public enum Roles {
         Leader,
         Officer,
